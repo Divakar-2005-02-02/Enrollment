81ec91eb78f5d2e10a5b845d9d9d3a0c2ab99239
Divakar S <divakardivakar30057@gmail.com>
2024-12-14 11:26:53 +0530

    Commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Enrollment/',  // Specify the repository name here
+  base: '/Enrollment/Frontend',  // Specify the repository name here
   plugins: [react()],
 });
